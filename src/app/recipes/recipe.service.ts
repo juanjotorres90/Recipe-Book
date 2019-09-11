849ad6386f357ea907bff3cc0f891d33e6689d68
Juanjo <juanjotorres90@gmail.com>
2019-09-11 21:14:00 +0200

    Observables

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,26 +1,29 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
-
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
+  recipeSelected = new Subject<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Tasty Schnitzel', 'A super-tasty Snchnitzel', '../../assets/img/schnitzel.jpg', [
-      new Ingredient('Meat', 1),
-      new Ingredient('French Fries', 20)
-    ]),
-    new Recipe('Big Fat Burger', 'What else you need to say?', '../../assets/img/burger.png', [
-      new Ingredient('Buns', 2),
-      new Ingredient('Meat', 1)
-    ])
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Snchnitzel',
+      '../../assets/img/schnitzel.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      '../../assets/img/burger.png',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    )
   ];
 
-constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
@@ -33,5 +36,4 @@ constructor(private slService: ShoppingListService) {}
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
-
 }
