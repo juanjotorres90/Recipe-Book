eb6c9028ee869f6b8010c16dce5591a13ea98f1f
Juanjo <juanjotorres90@gmail.com>
2019-11-18 21:44:46 +0100

    Sign Up Working

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    // tslint:disable-next-line: max-line-length
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDE0l2eF7sSqFTCuZdyWQiPy7pSFQeKpgE',
+    {
+      email,
+      password,
+      returnSecureToken: true
+    });
+  }
+}
