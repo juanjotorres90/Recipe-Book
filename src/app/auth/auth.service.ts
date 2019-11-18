7a8453a22b595e7ce3ebbdbbd0d37cbae8e40556
Juanjo <juanjotorres90@gmail.com>
2019-11-19 00:05:26 +0100

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,26 +1,65 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
     // tslint:disable-next-line: max-line-length
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDE0l2eF7sSqFTCuZdyWQiPy7pSFQeKpgE',
-    {
-      email,
-      password,
-      returnSecureToken: true
-    });
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDE0l2eF7sSqFTCuZdyWQiPy7pSFQeKpgE',
+      {
+        email,
+        password,
+        returnSecureToken: true
+      }
+    ).pipe(
+      catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    // tslint:disable-next-line: max-line-length
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDE0l2eF7sSqFTCuZdyWQiPy7pSFQeKpgE',
+        {
+          email,
+          password,
+          returnSecureToken: true
+        }
+      ).pipe(
+        catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
